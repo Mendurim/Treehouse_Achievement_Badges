ffb11172da4bb4b930871df770a05a4a945dc9d4
mendurim <mendurim.r@gmail.com>
2016-03-22 19:49:43 +0100

    Added the script.js file

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+  getData();  
+  function getData(){
+
+  $('form').submit(function (evt) {
+    var $submitButton = $('#submit');
+    var $searchField = $('#search');
+    evt.preventDefault();
+    $searchField.prop("disabled",true);
+    $submitButton.attr("disabled", true).val("searching....");
+    var animal = $searchField.val();
+    $('#photos').html('');
+    var APIurl = "https://teamtreehouse.com/"+animal+".json";
+    // debugger
+    $.ajax({
+        url : APIurl, 
+        dataType : 'json', 
+        type: 'get',
+        cache: false, 
+        success: function(data){
+          var photoHTML = ''; 
+          // if(data.name>0){
+          $.each(data.badges,function(i,photo){
+            //console.log(data.badges[index].icon_url); 
+            var badges = data.badges[i].icon_url; 
+            photoHTML += '<li class="grid-25 tablet-grid-50">';
+            photoHTML += '<a href="' + badges + '" class="image">';
+            photoHTML += '<img src="' + badges + '"></a></li>';
+
+          });
+          // }else{
+          //   photoHTML = "<p>No badges found that match: " + animal + ".</p>"
+          // }// end of if statement 
+          $('#photos').html(photoHTML); 
+          $searchField.prop("disabled", false);
+          $submitButton.attr("disabled", false).val("Search");
+        } //success function data
+      }); // end of ajax 
+
+    }); // end of function getData
+  }
+}); // end ready
+
+//     $.getJSON(APIurl, {
+//         tags: animal,
+//         format: "json"
+//       },
+//     function(data){
+//       var photoHTML = '';
+//       if (data.items.length > 0) {
+//         $.each(data.items,function(i,photo) {
+//           photoHTML += '<li class="grid-25 tablet-grid-50">';
+//           photoHTML += '<a href="' + photo.link + '" class="image">';
+//           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+//         }); // end each
+//       } else {
+//         photoHTML = "<p>No photos found that match: " + animal + ".</p>"
+//       }
+//       $('#photos').html(photoHTML);
+//       $searchField.prop("disabled", false);
+//       $submitButton.attr("disabled", false).val("Search");
+//     }); // end getJSON
+
+//   }); // end click
+
+
